996d5d11260845f65f21ecc29d7dc23e03203279
Chloe Wang <xywchloe@gmail.com>
2025-05-05 16:49:21 -0400

    added images

--- src/Temple.js
+++ src/Temple.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Temple.css';
 import { Link } from 'react-router-dom';
-import background from './assets/temple/temple_background.png';
-import fountain from './assets/temple/temple_fountain.png';
+import background from './assets/grass_background.png';
+import fountain from './assets/fountain_2.png';
 import moneyGod from './assets/temple/money_god.png';
-import mainGod from './assets/temple/main_god.png';
+import mainGod from './assets/main_god.png';
 import nftGod from './assets/temple/nft_god.png';
 import evilGod from './assets/temple/evil_god.png';
 
@@ -18,31 +18,25 @@ const Temple = () => {
       </Link>
 
       <div className="gods">
-        <div className="side-god">
-          <Link to="/money">
-            <img src={moneyGod} alt="Money God" />
-          </Link>
-        </div>
+       
         <div className="side-god">
           <Link to="/money2">
             <img src={moneyGod} alt="Money God 2" />
           </Link>
         </div>
+
         <div className="god">
           <Link to="/main">
-            <img src={mainGod} alt="Main God" />
+            <img src={mainGod} alt="Main God" className="temple-main-god"/>
           </Link>
         </div>
+        
         <div className="side-god">
           <Link to="/nft">
             <img src={nftGod} alt="NFT God" />
           </Link>
         </div>
-        <div className="side-god">
-          <Link to="/evil">
-            <img src={evilGod} alt="Evil God" />
-          </Link>
-        </div>
+        
       </div>
     </div>
   );
