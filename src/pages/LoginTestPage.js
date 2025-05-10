1791e7c043024318fd9a3c89ab31df405e62a62b
Chloe Wang <xywchloe@gmail.com>
2025-05-09 20:35:38 -0400

    added login bonus

---
+++ src/pages/LoginTestPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginBox from '../components/LoginBox';
+import './LoginTestPage.css';
+
+const LoginTestPage = () => {
+  return (
+    <div className="login-test-page">
+      <LoginBox />
+    </div>
+  );
+};
+
+export default LoginTestPage; 
